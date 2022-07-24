8cebdefd31b7ccae70dd6950d00a285dbdab4976
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-24 10:31:10 +0930

    Added WebpackPwaManifest to config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,6 +27,25 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: "src-sw.js",
       }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Leave code snippets and more in this text editor',
+        background_color: '#272822',
+        theme_color: '#272822',
+        start_url:'./',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
